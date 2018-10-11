f00beb97f1f82714e4689d77edb7020db6637265
uoon-dev <uoon.dev@gmail.com>
2018-10-11 18:57:59 +0900

    go to step02 - Drawing line

--- 02 - Drawing line/canvas-script.js
+++ 02 - Drawing line/canvas-script.js
@@ -5,5 +5,10 @@ window.onload = () => {
     const context = canvas.getContext('2d');
 
     // Draw line steps
-    context.beginPath();
+    context.beginPath(); // reset the context state
+    context.strokeStyle = 'red'; // color of the line
+    context.lineWidth = 10; // thickness of the line
+    context.moveTo(30, 70); // moveTo(x, y) -> starting point of line
+    context.lineTo(130, 70); // line(x, y) -> end point of the line
+    context.stroke(); // draws the line
 }
